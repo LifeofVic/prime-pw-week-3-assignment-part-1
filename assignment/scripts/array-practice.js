2a17c4f525cb00bcda99d310369448f344d9adcb
Victor Llapa <victorllapa@gmail.com>
2022-10-04 20:39:06 -0500

    Completed all section to Part 1 of week 3 assignment.

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -84,20 +84,27 @@ console.log('Updated favorite food list is: ' , favFoods);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
-
-
-
-
+favFoods[1] ="pasta"; 
+console.log("Replaced the second item on list with (pasta), removed (cheeseburger)" , favFoods) ;
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
+favFoods.sort();
+favFoods.reverse(); 
+console.log('Favorite food list in reverse alphabetical order: ' , favFoods);
+
+
 
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+
+console.log('Will display food list as a string', favFoods.join(" and "));
+
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+console.log('Combining favFood array and animalArray array: ', favFoods.concat(animalArray));
