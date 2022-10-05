5ddf9149fd292d855fed04b850aa32cf2c4fbc59
Victor Llapa <victorllapa@gmail.com>
2022-10-04 19:19:49 -0500

    Completed #3 of part 1.

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -31,15 +31,14 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
-
+console.log('Second animal is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-
+console.log('Last animal is, (using array index) ', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-
-
+console.log('Last animal is, (using array length)', animalArray[animalArray.length - 1]);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
